0a708d060637aa1d9ea964673bad558cef3e9c3b
Andrea <74471618+tedemango@users.noreply.github.com>
2021-07-06 23:27:12 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function pasapalabra() {
 	let str = document.getElementById("input").value;
-	string = str.replace(/a|e|o|u/gi,"i");
-	string = string + " :("
-	document.getElementById("input").value = string
+	string = str.replace(/a|e|o|u/g,"i");
+	strong = str.replace(/A|E|O|U/g,"I");
+	strong = strong + " :("
+	document.getElementById("input").value = strong
 }
 
 function color() {
