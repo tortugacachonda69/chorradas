d1922dde8b9983192444f02f8ba6a63b9058d06f
Andrea <74471618+tedemango@users.noreply.github.com>
2021-07-06 23:59:55 +0200

    update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function pasapalabra() {
 	let str = document.getElementById("input").value;
 	string = str.replace(/a|e|o|u/g,"i");
-	strong = str.replace(/A|E|O|U/g,"I");
+	strong = string.replace(/A|E|O|U/g,"I");
 	strong = strong + " :("
-	document.getElementById("input").value = strong
+	document.getElementById("salida").value = strong
 }
 
 function color() {
