8f20c16a7c9a868e197ec8a88103d81dabdc7ac4
Andrea <74471618+tedemango@users.noreply.github.com>
2021-07-18 00:52:54 +0200

    update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function teQuiero() {
 }
 
 function frase() {
-  let palabra = ["pene","amparo","cerveza","vasito","de","agua","chernobil","concentración","mearse","en","la","boca","un","pollo","semen","meter","bebe","fascista","los","canario","y","horchata","gusta","le","gasolina","condon","se","pone","el","follar","horno","no","si","nunca","alcohol","porro","desea","quiero","las","es","está","soy","eres","te","a","tiene","para","de","mañana","hoy"];
+  const palabra = ["pene","amparo","cerveza","vasito","de","agua","chernobil","concentración","mearse","en","la","boca","un","pollo","semen","meter","bebe","fascista","los","canario","y","horchata","gusta","le","gasolina","condon","se","pone","el","follar","horno","no","si","nunca","alcohol","porro","desea","quiero","las","es","está","soy","eres","te","a","tiene","para","de","mañana","hoy"];
   posicion = Math.floor(Math.random() * palabra.lenght);
   document.getElementById("frase").innerHTML = document.getElementById("frase").innerHTML + " " + palabra[posicion];
   }
