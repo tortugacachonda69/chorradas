93fe87c4d3d5f8fd71f8a928137e2f824f955b5e
Andrea <74471618+tedemango@users.noreply.github.com>
2021-07-18 10:37:06 +0200

    update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function teQuiero() {
 
 function frase() {
   const palabra = ["pene","amparo","cerveza","vasito","de","agua","chernobil","concentración","mearse","en","la","boca","un","pollo","semen","meter","bebe","fascista","los","canario","y","horchata","gusta","le","gasolina","condon","se","pone","el","follar","horno","no","si","nunca","alcohol","porro","desea","quiero","las","es","está","soy","eres","te","a","tiene","para","de","mañana","hoy","director","gazpacho","tu madre","puta","comprar","anticapitalista","bebes","anarquista","Kurti","necesitamos","sacar","algo","nuevo","colectivo","lgbt","que","sea","algun","hacer","tecito","csgo","empresa","dinosaurio"];
-  longitud = palabra.lenght
+  let longitud = palabra.lenght
   alert(longitud)
   posicion = Math.floor(Math.random() * 69);
   document.getElementById("frase").innerHTML = document.getElementById("frase").innerHTML + " " + palabra[posicion];
