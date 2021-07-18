64d38e6dcb51e71be82bdbc42330253f31a0d315
Andrea <74471618+tedemango@users.noreply.github.com>
2021-07-18 18:21:03 +0200

    update script.js

--- script.js
+++ script.js
@@ -71,30 +71,30 @@ function influencia() {
 const interrogacion = ["yo q se tus muertos", "callate ya ermano me tienes ya asta la polla", "si si claro ajajajaj"];
 
 const hola = ["ni hola ni hostias","hola caracola","ey","hasta luego"];
-const acho = ["no soy un robot soy un bebe aquí atrapado", "que dices yo me llamo Serg.io","vale si soy un bot que pasa?"]
-const otraCosa = ["lo que tu digas","seguro?","ok boomer","bueno vale callate","dame de comer","me parece perfecto","ya..."]
-const follar = ["no se que es eso de follar","la tengo enorme","soy un bebé no me hables de eso pederasta"]
-const bebe = ["si soy un bebé","soy un bb acabo de nacer"]
+const acho = ["no soy un robot soy un bebe aquí atrapado", "que dices yo me llamo Serg.io","vale si soy un bot que pasa?"];
+const otraCosa = ["lo que tu digas","seguro?","ok boomer","bueno vale callate","dame de comer","me parece perfecto","ya..."];
+const follar = ["no se que es eso de follar","la tengo enorme","soy un bebé no me hables de eso pederasta"];
+const bebe = ["si soy un bebé","soy un bb acabo de nacer"];
 
 function bebe() {
-let mensaje = document.getElementById("entrada").value;
-
-if (mensaje.includes("?")){
-reply = cadenaRandom(interrogacion);
-}
-else if (mensaje.includes("hola")){
-reply = cadenaRandom(hola);
-}
-else if (mensaje.includes("follar")){
-reply = cadenaRandom(follar);
-}
-else if (mensaje.includes("bebe")){
-reply = cadenaRandom(bebe);
-}
-else {
-reply = cadenaRandom(otraCosa);
-}
-document.getElementById("respuesta").innerHTML = reply;
+  let mensaje = document.getElementById("entrada").value;
+  
+  if (mensaje.includes("?")){
+  reply = cadenaRandom(interrogacion);
+  }
+  else if (mensaje.includes("hola")){
+  reply = cadenaRandom(hola);
+  }
+  else if (mensaje.includes("follar")){
+  reply = cadenaRandom(follar);
+  }
+  else if (mensaje.includes("bebe")){
+  reply = cadenaRandom(bebe);
+  }
+  else {
+  reply = cadenaRandom(otraCosa);
+  }
+  document.getElementById("respuesta").innerHTML = reply;
 }
 
 function cadenaRandom(nombreCadena) {
