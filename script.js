d1d07bb0f4e93e5e20fe8a7f8ea1a2a1c658036e
Andrea <74471618+tedemango@users.noreply.github.com>
2021-07-18 18:16:38 +0200

    update script.js

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ const interrogacion = ["yo q se tus muertos", "callate ya ermano me tienes ya as
 
 const hola = ["ni hola ni hostias","hola caracola","ey","hasta luego"];
 const acho = ["no soy un robot soy un bebe aquí atrapado", "que dices yo me llamo Serg.io","vale si soy un bot que pasa?"]
-const otraCosa = ["lo que tu digas","seguro?","ok boomer","bueno vale callate","dame de comer","me parece perfecto"]
+const otraCosa = ["lo que tu digas","seguro?","ok boomer","bueno vale callate","dame de comer","me parece perfecto","ya..."]
 const follar = ["no se que es eso de follar","la tengo enorme","soy un bebé no me hables de eso pederasta"]
 const bebe = ["si soy un bebé","soy un bb acabo de nacer"]
 
